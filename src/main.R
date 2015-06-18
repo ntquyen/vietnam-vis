b42ac49daaa287d966f0f1a32ae5263ae5dbddde
ntquyen <nguyen.tuan.quyen.it@gmail.com>
2015-06-18 07:22:58 +0700

    Remove test file

--- src/main.R
+++ src/main.R
@@ -7,7 +7,7 @@ names(data) <- c("province", "area.km2", "population", "density")
 province.codes <- read.csv("./data/vn_provinces_code.csv", header = FALSE)
 names(province.codes) <- c("iso3166.2", "province")
 data <- merge(data, province.codes, by = c("province"))
-df <- select(data, iso3166.2, density)
+
 geo <- gvisGeoChart(data, 
                   locationvar = "iso3166.2", 
                   colorvar = "density", hovervar = "province",
